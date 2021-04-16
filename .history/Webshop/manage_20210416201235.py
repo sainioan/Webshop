346aa17ccec4448b6a181c821254c14c24413082
sainioan <sainio.anniina@gmail.com>
2021-04-16 20:21:58 +0300

    change database to mysql

--- .history/Webshop/manage_20210416201235.py
+++ .history/Webshop/manage_20210416201235.py
@@ -3,10 +3,7 @@
 import os
 import sys
 
-import pymysql
 
-
-pymysql.install_as_MySQLdb()
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Webshop.settings')
