ffd48247e4f2d0bc6a334bdbefe5154d189d0129
Anniina Sainio <sainio.anniina@gmail.com>
2021-04-17 09:14:49 +0300

    Delete Webshop/.history directory

--- Webshop/.history/Webshop/views_20210417075521.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .forms import ContactForm
-from django.http import JsonResponse, HttpResponse
-
-
-def home_page(request):
-    return render(request, "home.html", {})
